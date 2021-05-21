76ed77bb3725e67275741a02a9c9c96a63241b3a
pierre edimo <pierredimo@live.com>
2021-05-21 22:52:29 +0200

    layout container

---
+++ components/layout.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import styles from "../styles/layout.module.css";
+import AppHeader from "./appheader";
+import AppFooter from "./appfooter"; 
+
+export default function Layout({ title, children }) {
+  return (
+    <div className={styles.layoutContainer}>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <AppHeader />
+      <div className={styles.container}>{children}</div>
+      <div>
+        <AppFooter/>
+      </div>
+    </div>
+  );
+}
+
+Layout.defaultProps = {
+  title: "Patrice Edimo",
+};
