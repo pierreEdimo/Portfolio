eb0b6ee2eea42dc46de047a4bd132e13ab4f0806
pierre edimo <pierredimo@live.com>
2021-05-21 22:50:25 +0200

    add appfooter

---
+++ components/appfooter.js
@@ -0,0 +1,9 @@
+import styles from "../styles/appfooter.module.css"; 
+
+export default function AppFooter() {
+    return (
+        <footer className={styles.footercontainer}>
+            <p>Copyright &copy; 2021</p>
+        </footer>
+    )
+}
